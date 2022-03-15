ab2f5bbaad2fd45a1db68d5515df0319d8492178
Sun Jiao <P.ozzy@outlook.com>
2022-03-15 22:05:30 +0800

    Update ebird_picture_downloader.user.js

--- ebird_picture_downloader.user.js
+++ ebird_picture_downloader.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/sun-jiao
 // @match          https://ebird.org/*
 // @grant          GM_xmlhttpRequest
-// @version        1.0
+// @version        1.1
 // @author       Sun Jiao
 // @license        GPL_v3
 // @description    Display a banner to download the picture on ebird or view it in browser.
@@ -15,12 +15,20 @@ const SUFFIX = '/2400';
 
 function link_func() {
     var ul_list = document.getElementsByClassName('UnorderedList UnorderedList--flat');
+    if(ul_list.length == 0){
+        console.log('error1');
+        return;}
     var lis = ul_list[ul_list.length - 1].getElementsByTagName("li");
-    var a = lis[0].getElementsByTagName("a")[0];
-    var href = a.getAttribute("href");
-    var id = href.split("-")[1];
+    if(lis.length < 2){
+        console.log('error2');
+        return;}
+    var a0 = lis[0].getElementsByTagName("a")[0];
+    var a2 = lis[lis.length - 1].getElementsByTagName("a")[0];
+    var href = a2.getAttribute("href");
+    var splits = href.split("/");
+    var id = splits[splits.length - 1];
     if(typeof id !== 'undefined' && ! id.includes(".")){
-        a.setAttribute("href", PREFIX + id + SUFFIX);
+        a0.setAttribute("href", PREFIX + id + SUFFIX);
     }
     console.log("link");
 }
